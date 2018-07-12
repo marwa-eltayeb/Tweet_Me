0cd5e6237648bac194ee042e8cf618d7519dd2bb
Marwa-Said <marwa_eltayeb@yahoo.com>
2018-07-12 17:06:31 +0200

    Tweets

---
+++ app/src/main/java/com/example/marwa/tweetme/LoginActivity.java
@@ -0,0 +1,101 @@
+package com.example.marwa.tweetme;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthToken;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+
+    //Declaring Twitter loginButton object
+    TwitterLoginButton loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //Initializing twitter instance
+        Twitter.initialize(this);
+        //Make sure that this statement is added before setContentView() method
+        setContentView(R.layout.activity_login);
+
+
+        //Instantiating loginButton
+        loginButton = (TwitterLoginButton) findViewById(R.id.login_button);
+
+
+
+         /*
+          Adding a callback to loginButton
+          These statements will execute when loginButton is clicked
+         */
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                /*
+                  This provides TwitterSession as a result
+                  This will execute when the authentication is successful
+                 */
+                TwitterSession session = TwitterCore.getInstance().getSessionManager().getActiveSession();
+                TwitterAuthToken authToken = session.getAuthToken();
+                String token = authToken.token;
+                String secret = authToken.secret;
+
+                // Calling login method and passing twitter session
+                login(session);
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                // Displaying Toast message
+                Toast.makeText(LoginActivity.this, "Authentication failed!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+    }
+
+
+    /**
+     * @param session
+     * This method will get username using session and start a new activity where username will be displayed
+     */
+    public void login(TwitterSession session)
+    {
+        String username = session.getUserName();
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        intent.putExtra("username", username);
+        startActivity(intent);
+    }
+
+    /**
+     * @param requestCode - we'll set it to REQUEST_CAMERA
+     * @param resultCode - this will store the result code
+     * @param data - data will store an intent
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result to the login button.
+        loginButton.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+
+
+
+
+}
